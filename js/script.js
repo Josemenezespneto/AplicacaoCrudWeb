9e16f97319c5b330ed2c7dcae4c21daa6b8908e9
Josemenezespneto <menezespn@gmail.com>
2024-01-29 19:43:23 -0300

    alteracao na index class por id e alteracao no script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert();
+var linhaSelecionada = null;
+
+// Alertas
+function alerta(mensagem, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(mensagem));
+
+    const container = document.querySelector(".container");
+    const main = document.querySelector(".main");
+    container.insertBefore(div, main);
+
+    setTimeout(() => div.remove(), 3000);
+}
+
+// Deletar
+document.querySelector("#form-aluno").addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (target.contains("delete")) {
+        const linha = target.parentElement.parentElement;
+        linha.remove();
+        alerta("Aluno excluído com sucesso!", "danger");
+    }
+});
