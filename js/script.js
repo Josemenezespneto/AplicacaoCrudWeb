cce837821d2b0707823d0eb77674960b6c95a91c
Josemenezespneto <menezespn@gmail.com>
2024-01-29 23:50:49 -0300

    AplicacaoCrud -4 alteraçao no index ids e alteraçao no script js,conserto da funcao deletar

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
-var linhaSelecionada = null;
+var selectedRow = null;
 
-// Alertas
-function alerta(mensagem, className) {
+//show alerts
+function showAlert(message, className){
     const div = document.createElement("div");
     div.className = `alert alert-${className}`;
-    div.appendChild(document.createTextNode(mensagem));
 
+    div.appendChild(document.createTextNode(message));
     const container = document.querySelector(".container");
     const main = document.querySelector(".main");
-    container.insertBefore(div, main);
+    container.insertBefore(div,main);
 
-    setTimeout(() => div.remove(), 3000);
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+
 }
 
-// Deletar
-document.querySelector("#form-aluno").addEventListener("click", (e) => {
-    const target = e.target;
-
-    if (target.contains("delete")) {
-        const linha = target.parentElement.parentElement;
-        linha.remove();
-        alerta("Aluno excluído com sucesso!", "danger");
+document.querySelector("#lista-aluno").addEventListener("click", (e) =>{
+    target= e.target;
+    if (target.classList.contains("delete")) {
+        target.parentElement.parentElement.remove();
+        showAlert("Aluno removido com sucesso!", "danger");
     }
 });
+ g
